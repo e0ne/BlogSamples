65bd04dd8d0c6df8748b8b274a3ae1682fc38ca1
Ivan Kolodyazhny <e0ne@e0ne.info>
2011-09-25 21:35:33 +0300

    #[Fix #18779539] Add sample

---
+++ GitPivotalPylint/models.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.db import models
+
+# Create your models here.
+
+class BaseModel(models.Model):
+    POSITION = (
+        (1, 'First'),
+        (2, 'Second'),
+        (3, 'Third'),
+    )
+
+    name = models.CharField(max_length=20)
+    position = models.IntegerField(choices=POSITION)
+
+
+admin.site.register(BaseModel)
